13641721999d2c0f0ab0fe9bc0e6309a1a32d456
Anand Bajpai <anand.bajpai@1mg.com>
2020-12-23 16:27:29 +0530

    Initial commit

---
+++ ColorFontModule/ColorFontModule.h
@@ -0,0 +1,18 @@
+//
+//  ColorFontModule.h
+//  ColorFontModule
+//
+//  Created by anand on 23/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ColorFontModule.
+FOUNDATION_EXPORT double ColorFontModuleVersionNumber;
+
+//! Project version string for ColorFontModule.
+FOUNDATION_EXPORT const unsigned char ColorFontModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorFontModule/PublicHeader.h>
+
+
